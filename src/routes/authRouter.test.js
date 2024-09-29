9bd23fdc30e23fd32463f48dd25beb6ac1787bc5
allis <allison.nhart@yahoo.com>
2024-09-29 16:08:46 -0600

    added lint, config, and yml

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,48 @@
-test('hello world', () => {
-    expect('hello' + ' world').toBe('hello world');
-  });
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expect(testUserAuthToken).toBe(registerRes.body.token);
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(password).toBe('a');
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+  }
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
+  const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  return user;
+}
+
+test('implementing createAdminUser', async () => {
+    let user = createAdminUser();
+    expect(user).toEqual({});
+})
