475f26c2e0f53d7d7c0ca19c9ef6b2f57671deb4
allis <allison.nhart@yahoo.com>
2024-10-23 15:12:32 -0600

    updated yml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,13 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.js"], languageOptions: {
+    sourceType: "module",
+    globals: {
+      ...globals.node,
+      ...globals.browser
+    }
+  }},
   {languageOptions: { globals: globals.node }},
   {languageOptions: { globals: globals.jest }},
   pluginJs.configs.recommended,
